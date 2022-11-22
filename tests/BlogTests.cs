03da20c31035c0107a5c1b994eba70de87ffd573
Christian Helle <christian.helle@outlook.com>
2022-11-22 23:25:33 +0100

    Click cookie consent in blog tests

--- tests/BlogTests.cs
+++ tests/BlogTests.cs
@@ -1,5 +1,6 @@
 using Microsoft.Playwright;
 using Microsoft.Playwright.NUnit;
+using System.Diagnostics;
 
 namespace PlaywrightTests;
 
@@ -11,32 +12,38 @@ public class BlogTests : PageTest
     public async Task Main_Page()
     {
         const string url = "https://christianhelle.com";
-        
-        var browser = await Playwright.Firefox.LaunchAsync();
+
+        var browser = await Playwright.Firefox.LaunchAsync(new BrowserTypeLaunchOptions { Headless = Debugger.IsAttached });
         var context = await browser.NewContextAsync(new BrowserNewContextOptions { AcceptDownloads = true });
         var page = await context.NewPageAsync();
         await page.GotoAsync(url);
+        await page.ClickAsync("a[id='cookie-notice-accept']");
+        await page.ClickAsync("span[class='IL_AD']");
     }
 
     [Test]
     public async Task Autofaker()
     {
         const string url = "https://christianhelle.com/2022/10/autofaker";
-        
-        var browser = await Playwright.Firefox.LaunchAsync();
+
+        var browser = await Playwright.Firefox.LaunchAsync(new BrowserTypeLaunchOptions { Headless = !Debugger.IsAttached });
         var context = await browser.NewContextAsync(new BrowserNewContextOptions { AcceptDownloads = true });
         var page = await context.NewPageAsync();
         await page.GotoAsync(url);
+        await page.ClickAsync("a[id='cookie-notice-accept']");
+        await page.ClickAsync("span[class='IL_AD']");
     }
 
     [Test]
     public async Task Orchestrated_ETL()
     {
         const string url = "https://christianhelle.com/2022/09/orchestrated-etl";
-        
-        var browser = await Playwright.Firefox.LaunchAsync();
+
+        var browser = await Playwright.Firefox.LaunchAsync(new BrowserTypeLaunchOptions { Headless = !Debugger.IsAttached });
         var context = await browser.NewContextAsync(new BrowserNewContextOptions { AcceptDownloads = true });
         var page = await context.NewPageAsync();
         await page.GotoAsync(url);
+        await page.ClickAsync("a[id='cookie-notice-accept']");
+        await page.ClickAsync("span[class='IL_AD']");
     }
 }
