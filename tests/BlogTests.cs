57f83ab167157e95a04cc66072b14a1768f09816
Christian Helle <christian.helle@outlook.com>
2022-11-23 11:40:39 +0100

    Only show browser UI when in DEBUG build

--- tests/BlogTests.cs
+++ tests/BlogTests.cs
@@ -1,6 +1,5 @@
 using Microsoft.Playwright;
 using Microsoft.Playwright.NUnit;
-using System.Diagnostics;
 
 namespace PlaywrightTests;
 
@@ -8,42 +7,51 @@ namespace PlaywrightTests;
 [TestFixture]
 public class BlogTests : PageTest
 {
+    private readonly BrowserTypeLaunchOptions browserTypeLaunchOptions = new BrowserTypeLaunchOptions
+    {
+        ChromiumSandbox = true,
+#if DEBUG
+        Headless = false,
+#endif
+    };
+
     [Test]
-    public async Task Main_Page()
+    public async Task Main_Page_Open_AutoFaker()
     {
         const string url = "https://christianhelle.com";
 
-        var browser = await Playwright.Firefox.LaunchAsync(new BrowserTypeLaunchOptions { Headless = !Debugger.IsAttached });
+        var browser = await Playwright.Firefox.LaunchAsync(browserTypeLaunchOptions);
         var context = await browser.NewContextAsync(new BrowserNewContextOptions { AcceptDownloads = true });
         var page = await context.NewPageAsync();
         await page.GotoAsync(url);
         await page.ClickAsync("a[id='cookie-notice-accept']");
+        await page.ClickAsync("a[href='/2022/10/autofaker.html']");
         await page.ClickAsync("span[class='IL_AD']");
     }
 
     [Test]
-    public async Task Autofaker()
+    public async Task Main_Page_Open_Orchestrated_ETL()
     {
-        const string url = "https://christianhelle.com/2022/10/autofaker";
+        const string url = "https://christianhelle.com";
 
-        var browser = await Playwright.Firefox.LaunchAsync(new BrowserTypeLaunchOptions { Headless = !Debugger.IsAttached });
+        var browser = await Playwright.Firefox.LaunchAsync(browserTypeLaunchOptions);
         var context = await browser.NewContextAsync(new BrowserNewContextOptions { AcceptDownloads = true });
         var page = await context.NewPageAsync();
-        await page.GotoAsync(url);
+        await page.GotoAsync(url);        
         await page.ClickAsync("a[id='cookie-notice-accept']");
+        await page.ClickAsync("a[href='/2022/09/orchestrated-etl.html']");
         await page.ClickAsync("span[class='IL_AD']");
     }
 
     [Test]
-    public async Task Orchestrated_ETL()
+    public async Task Crawl_Archive()
     {
-        const string url = "https://christianhelle.com/2022/09/orchestrated-etl";
+        const string url = "https://christianhelle.com/archives";
 
-        var browser = await Playwright.Firefox.LaunchAsync(new BrowserTypeLaunchOptions { Headless = !Debugger.IsAttached });
+        var browser = await Playwright.Firefox.LaunchAsync(browserTypeLaunchOptions);
         var context = await browser.NewContextAsync(new BrowserNewContextOptions { AcceptDownloads = true });
         var page = await context.NewPageAsync();
         await page.GotoAsync(url);
-        await page.ClickAsync("a[id='cookie-notice-accept']");
-        await page.ClickAsync("span[class='IL_AD']");
+        var selectors = await page.QuerySelectorAllAsync("a");
     }
 }
