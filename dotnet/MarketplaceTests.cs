5848fccab727e9d062875318940fa410495f42ba
Christian Helle <christian.helle@outlook.com>
2022-11-24 16:53:49 +0100

    Refactor marketplace tests

--- dotnet/MarketplaceTests.cs
+++ dotnet/MarketplaceTests.cs
@@ -7,6 +7,26 @@ namespace PlaywrightTests;
 [TestFixture]
 public class MarpletplaceTests : PageTest
 {
+    private readonly BrowserTypeLaunchOptions browserTypeLaunchOptions = new BrowserTypeLaunchOptions
+    {
+        ChromiumSandbox = true,
+#if DEBUG
+        Headless = false,
+#endif
+    };
+
+    private readonly BrowserNewContextOptions options =
+        new BrowserNewContextOptions
+        {
+            AcceptDownloads = true
+        };
+
+#if DEBUG
+    private readonly int MaxAttempts = 1;
+#else
+    private int MaxAttempts = 100;
+#endif
+
     [Test]
     public async Task Download_APIClientCodeGenerator_For_VS2019()
     {
@@ -21,6 +41,7 @@ public class MarpletplaceTests : PageTest
         const string url = "https://marketplace.visualstudio.com/items?itemName=ChristianResmaHelle.APIClientCodeGenerator2022";
         const string output = "APIClientCodeGenerator2022.vsix";
         await Download(url, output);
+        Assert.True(File.Exists(output));
     }
 
     [Test]
@@ -29,19 +50,28 @@ public class MarpletplaceTests : PageTest
         const string url = "https://marketplace.visualstudio.com/items?itemName=ChristianResmaHelle.ResWFileCodeGenerator";
         const string output = "ResWFileCodeGenerator.vsix";
         await Download(url, output);
+        Assert.True(File.Exists(output));
     }
 
     private async Task Download(string url, string output)
     {
-        var browser = await Playwright.Firefox.LaunchAsync();
-        var context = await browser.NewContextAsync(new BrowserNewContextOptions { AcceptDownloads = true });
-        var page = await context.NewPageAsync();
-        await page.GotoAsync(url);
-        var waitForDownloadTask = page.WaitForDownloadAsync();
-        var button = page.Locator("button:has-text('Download')");
-        await button.ClickAsync();
-        var download = await waitForDownloadTask;
-        await download.SaveAsAsync(output);
-        Assert.True(File.Exists(output));
+        var random = new Random();
+        var length = random.Next(1, MaxAttempts);
+        for (int i = 0; i < length; i++)
+        {
+            var browser = await Playwright.Chromium.LaunchAsync(browserTypeLaunchOptions);
+            var context = await browser.NewContextAsync(options);
+            var page = await context.NewPageAsync();
+            await page.GotoAsync(url);
+
+            if (random.Next(2) != 0)
+                continue;
+
+            var waitForDownloadTask = page.WaitForDownloadAsync();
+            var button = page.Locator("button:has-text('Download')");
+            await button.ClickAsync();
+            var download = await waitForDownloadTask;
+            await download.SaveAsAsync(output);
+        }
     }
 }
