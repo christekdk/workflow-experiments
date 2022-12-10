8609c938bcc8acf0fd41e44e4246b0bab9f683d0
Christian Helle <christian.helle@outlook.com>
2022-12-10 14:52:11 +0100

    Update Github tests

--- dotnet/GIthubTests.cs
+++ dotnet/GIthubTests.cs
@@ -28,23 +28,35 @@ public class GithubTests : PageTest
     [Test]
     public async Task SqlCeQueryAnalyzer_Download()
     {
-        var browser = await Playwright.Chromium.LaunchAsync(browserTypeLaunchOptions);
-        var context = await browser.NewContextAsync(options);
-        
-        var page = await context.NewPageAsync();
-        await page.GotoAsync("https://github.com/christianhelle");
-
-        await page.GetByRole(AriaRole.Link, new() { NameString = "sqlcequery" }).ClickAsync();
-        await page.WaitForURLAsync("https://github.com/christianhelle/sqlcequery");
-
-        await page.GetByRole(AriaRole.Link, new() { NameString = "1.2.6626 Latest May 1, 2021on May 1, 2021" }).ClickAsync();
-        await page.WaitForURLAsync("https://github.com/christianhelle/sqlcequery/releases/tag/1.2.6626");
-        
-        await page.Mouse.WheelAsync(0, 1000);
-
-        await page.RunAndWaitForDownloadAsync(async () =>
+        var random = new Random();
+        var length = random.Next(1, MaxAttempts);
+        for (int i = 0; i < length; i++)
         {
-            await page.GetByRole(AriaRole.Link, new() { NameString = "SQL.Compact.Query.Analyzer.Setup.v1.2.6626.exe" }).ClickAsync();
-        });
+            var browser = await (i % 2 == 0 ? Playwright.Firefox : Playwright.Chromium).LaunchAsync(browserTypeLaunchOptions);
+            var context = await browser.NewContextAsync(options);
+
+            var page = await context.NewPageAsync();
+            await page.GotoAsync("https://github.com/christianhelle");
+
+            await page.GetByRole(AriaRole.Link, new() { NameString = "sqlcequery" }).ClickAsync();
+            await page.WaitForURLAsync("https://github.com/christianhelle/sqlcequery");
+
+            await page.GetByRole(AriaRole.Link, new() { NameString = "1.2.6626 Latest May 1, 2021on May 1, 2021" }).ClickAsync();
+            await page.WaitForURLAsync("https://github.com/christianhelle/sqlcequery/releases/tag/1.2.6626");
+
+            await page.Mouse.WheelAsync(0, 1000);
+
+            await page.RunAndWaitForDownloadAsync(async () =>
+            {
+                await page.GetByRole(AriaRole.Link, new() { NameString = "SQL.Compact.Query.Analyzer.Setup.v1.2.6626.exe" }).ClickAsync();
+            });
+            await page.WaitForURLAsync("https://github.com/christianhelle/sqlcequery/releases/tag/1.2.6626");
+
+            await page.RunAndWaitForDownloadAsync(async () =>
+            {
+                await page.GetByRole(AriaRole.Link, new() { NameString = "SQL.Compact.Query.Analyzer.v1.2.6626.zip" }).ClickAsync();
+            });
+            await page.WaitForURLAsync("https://github.com/christianhelle/sqlcequery/releases/tag/1.2.6626");
+        }
     }
 }
