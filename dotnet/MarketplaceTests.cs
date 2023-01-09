3f517bd3ec898bcf27bff9aafa67855ea4d2a71a
Christian Helle <christian.helle@outlook.com>
2023-01-09 15:43:56 +0100

    Add VS2017 tests

--- dotnet/MarketplaceTests.cs
+++ dotnet/MarketplaceTests.cs
@@ -28,6 +28,15 @@ public class MarpletplaceTests : PageTest
 #endif
 
     [Test]
+    public async Task Download_APIClientCodeGenerator2022_For_VS2017()
+    {
+        const string url = "https://marketplace.visualstudio.com/items?itemName=ChristianResmaHelle.APIClientCodeGenerator2017";
+        const string output = "vs2017.vsix";
+        await Download(url, output);
+        Assert.True(File.Exists(output));
+    }
+
+    [Test]
     public async Task Download_APIClientCodeGenerator_For_VS2019()
     {
         const string url = "https://marketplace.visualstudio.com/items?itemName=ChristianResmaHelle.APIClientCodeGenerator";
@@ -36,23 +45,23 @@ public class MarpletplaceTests : PageTest
         Assert.True(File.Exists(output));
     }
 
-    // [Test]
-    // public async Task Download_APIClientCodeGenerator2022_For_VS2022()
-    // {
-    //     const string url = "https://marketplace.visualstudio.com/items?itemName=ChristianResmaHelle.APIClientCodeGenerator2022";
-    //     const string output = "vs2022.vsix";
-    //     await Download(url, output);
-    //     Assert.True(File.Exists(output));
-    // }
+    [Test]
+    public async Task Download_APIClientCodeGenerator2022_For_VS2022()
+    {
+        const string url = "https://marketplace.visualstudio.com/items?itemName=ChristianResmaHelle.APIClientCodeGenerator2022";
+        const string output = "vs2022.vsix";
+        await Download(url, output);
+        Assert.True(File.Exists(output));
+    }
 
-    // [Test]
-    // public async Task Download_ResWFileCodeGenerator()
-    // {
-    //     const string url = "https://marketplace.visualstudio.com/items?itemName=ChristianResmaHelle.ResWFileCodeGenerator";
-    //     const string output = "ResWFileCodeGenerator.vsix";
-    //     await Download(url, output);
-    //     Assert.True(File.Exists(output));
-    // }
+    [Test]
+    public async Task Download_ResWFileCodeGenerator()
+    {
+        const string url = "https://marketplace.visualstudio.com/items?itemName=ChristianResmaHelle.ResWFileCodeGenerator";
+        const string output = "ResWFileCodeGenerator.vsix";
+        await Download(url, output);
+        Assert.True(File.Exists(output));
+    }
 
     private async Task Download(string url, string output, int maxAttempts = MaxAttempts)
     {
