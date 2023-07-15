0d1564f7685a8cf0a3e96c1877e259230585e4d7
Christian Helle <christian.helle@outlook.com>
2023-07-15 10:26:58 +0200

    Missing namespace

--- dotnet/GIthubTests.cs
+++ dotnet/GIthubTests.cs
@@ -1,6 +1,8 @@
 using Microsoft.Playwright.NUnit;
 using Microsoft.Playwright;
 
+namespace PlaywrightTests;
+
 [Parallelizable(ParallelScope.Self)]
 [TestFixture]
 public class GithubTests : PageTest
